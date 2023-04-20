7a0f0ade7ca4655e706edd6163bdf88e42d6df70
Albert Rosa <arosa6@usf.edu>
2023-04-19 21:19:50 -0400

    Sales and Customer files added

---
+++ frontend/Add_delete_Customer.py
@@ -0,0 +1,65 @@
+#This File will handle the building and maintaining of the "Maintenance" Entity
+from backend.db import connect
+from backend.queries.add_queries import *
+from backend.queries.find_queries import *
+from backend.queries.delete_queries import *
+from tkinter import *
+import datetime
+import tkinter as tk 
+from tkinter import StringVar, IntVar, Button, Label, Entry, Toplevel
+
+
+
+class Vehicle_Sells(Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master = master)
+        self.title("Sales")
+        self.geometry("1500x1200")
+        self.connection = connect()
+        #Parameters to build the maintenance item
+        self.vin = StringVar()
+        self.c_name = StringVar()
+        self.c_email = StringVar()
+        self.e_id = IntVar()
+        self.c_addr = StringVar()
+        self.phone = IntVar()
+        self.year = IntVar()
+        self.month = IntVar()
+        self.day = IntVar()
+        self.value = True
+        #Build the outline of the date requirements
+        self.customer_date_label = Label(self, text="Enter The DOB in day-month-year using numbers: dd-mm-yyyy:")
+        self.customer_date_label.grid(column=2, row=4, padx=(30,0), pady=(30,0))
+        self.customer_date_entry = Entry(self, width=2, textvariable=self.day)
+        self.customer_date_entry.grid(column=2,row=5,padx=(10,0),pady=(10,0))
+        self.customer_month_entry = Entry(self, width=2, textvariable=self.month)
+        self.customer_month_entry.grid(column=2, row=6, padx=(10,0), pady=(10,0))
+        self.customer_year_entry = Entry(self, width=4, textvariable=self.year)
+        self.customer_year_entry.grid(column=2, row=7, padx=(30,0), pady=(30,0))
+        self.customer_name_label = Label(self, text="Enter Customer Name:")
+        self.customer_name_label.grid(column=2, row=8, padx=(30,0), pady=(30,0))
+        self.customer_name_entry = Entry(self, width=30, textvariable=self.c_name)
+        self.customer_name_entry.grid(column=3, row=8, padx=(30,0), pady=(30,0))
+        self.customer_email_label = Label(self, text="Enter Customer Email:")
+        self.customer_email_label.grid(column=2, row=9, padx=(30,0), pady=(30,0))
+        self.customer_email_entry = Entry(self, width=30, textvariable=self.c_email)
+        self.customer_email_entry.grid(column=3, row=9, padx=(30,0), pady=(30,0))
+        self.customer_addr_label = Label(self, text="Enter Customer Address:")
+        self.customer_addr_label.grid(column=2, row=9, padx=(30,0), pady=(30,0))
+        self.customer_addr_entry = Entry(self, width=30, textvariable=self.c_email)
+        self.customer_addr_entry.grid(column=3, row=9, padx=(30,0), pady=(30,0))
+        self.customer_phone_label = Label(self, text="Enter Customer Phone Numnber:")
+        self.customer_phone_label.grid(column=2, row=9, padx=(30,0), pady=(30,0))
+        self.customer_phone_entry = Entry(self, width=30, textvariable=self.c_email)
+        self.customer_phone_entry.grid(column=3, row=9, padx=(30,0), pady=(30,0))
+        
+        self.customer_button = Button(self, text="Create", command=self.create_Customer)
+        self.customer_button.grid(column=2, row=10, padx=(30,0), pady=(30,0))
+        self.maintenance_button_del = Button(self, text="Delete", command=self.delete_Customer)
+        self.customer_button.grid(column=2, row=10, padx=(30,0), pady=(30,0))
+    def create_Customer(self):
+        #name, address, email, dob, p_no
+        date = datetime.date(self.year.get(), self.month.get(), self.day.get())
+        add_customer(self.connection, self.c_name, self.c_addr, self.c_email, date, self.phone)
+    def delete_Customer(self):
+        delete_customer(self.connection, self.c_name, self.c_email)
--- frontend/Sales.py
+++ frontend/Sales.py
@@ -10,56 +10,47 @@ from tkinter import StringVar, IntVar, Button, Label, Entry, Toplevel
 
 
 
-class Vehicle_add_delete(Toplevel):
+class Vehicle_Sells(Toplevel):
     def __init__(self, master=None):
         super().__init__(master = master)
-        self.title("Maintenance")
+        self.title("Sales")
         self.geometry("1500x1200")
         self.connection = connect()
         #Parameters to build the maintenance item
-        self.main_no = IntVar()
-        self.year = IntVar()
-        self.month = IntVar()
-        self.day = IntVar()
-        self.service = StringVar()
-        self.e_id = IntVar()
-        self.date = datetime.date()
         self.vin = StringVar()
+        self.c_name = StringVar()
+        self.c_email = StringVar()
+        self.e_id = IntVar()
+        #Build the outline of the customer requirements
+        self.customer_name_label = Label(self, text="Enter Customer Name:")
+        self.customer_name_label.grid(column=2, row=2, padx=(30,0), pady=(30,0))
+        self.customer_name_entry = Entry(self, width=30, textvariable=self.c_name)
+        self.customer_name_entry.grid(column=3, row=3, padx=(30,0), pady=(30,0))
+        self.customer_name_label = Label(self, text="Enter Customer Email:")
+        self.customer_name_label.grid(column=2, row=3, padx=(30,0), pady=(30,0))
+        self.customer_name_entry = Entry(self, width=30, textvariable=self.c_email)
+        self.customer_name_entry.grid(column=3, row=3, padx=(30,0), pady=(30,0))
+    
+        self.saleperson_id_label = Label(self, text="Enter your employee id:")
+        self.saleperson_id_label.grid(column=2, row=4, padx=(30,0), pady=(30,0))
+        self.saleperson_id_entry = Entry(self, width=30, textvariable=self.e_id)
+        self.saleperson_id_entry.grid(column=3, row=4, padx=(30,0), pady=(30,0))
+    
+        self.vehicle_id_label = Label(self, text="Enter the Vehicle VIN")
+        self.vehicle_id_label.grid(column=2, row=5, padx=(30,0), pady=(30,0))
+        self.vehicle_id_entry = Entry(self, width=30, textvariable=self.vin)
+        self.vehicle_id_entry.grid(column=3, row=5, padx=(30,0), pady=(30,0))
 
-        #Build the outline of the maintenance requirements
-        self.maintenance_date_label = Label(self, text="Enter The date in day-month-year using numbers: dd-mm-yyyy:")
-        self.maintenance_date_label.grid(column=2, row=3, padx=(30,0), pady=(30,0))
-        self.maintenance_date_entry = Entry(self, width=2, textvariable=self.day)
-        self.maintenance_date_entry.grid(column=2,row=4,padx=(10,0),pady=(10,0))
-        self.maintenance_month_entry = Entry(self, width=2, textvariable=self.month)
-        self.maintenance_month_entry.grid(column=2, row=5, padx=(10,0), pady=(10,0))
-        self.maintenance_year_entry = Entry(self, width=4, textvariable=self.year)
+        self.submit_button = Button(self, text="Submit", command=self.make_sale)
+        self.submit_button.grid(column=2, row=6, padx=(30,0), pady=(30,0))
 
-        self.maintenance_number_label = Label(self, text="Enter Maintenance ID: ")
-        self.maintenance_number_label.grid(column=2, row=6, padx=(20,0), pady=(20,0))
-        self.maintenance_number_entry = Entry(self, width=10, textvariable=self.e_id)
-        self.maintenance_number_entry.grid(column=3, row=6, padx=(20,0), pady=(20,0))
+    def make_sale(self):
+        cur = connection.cursor()
+        up_vehicle = """UPDATE Vehicle SET Customer_name = %s, Customer_email = %s WHERE VIN = %s;"""
+        cur.execute(up_vehicle, (self.c_name, self.c_email, self.vin,))
+        up_customer = """UPDATE Customer Set Seller = %s WHERE Customer_name = %s AND Customer_email = %s"""
+        cur.execute(up_customer, (self.e_id, self.c_name, self.c_email))
+        connection.commit()
+        return
 
-        self.maintenance_service_label = Label(self, text="Enter the type of service to preform: ")
-        self.maintenance_service_label.grid(column=2, row = 8, padx=(20,0), pady=(20,0))
-        self.maintenance_service_entry = Entry(self, width=20, textvariable=self.service)
-        self.maintenance_service_entry.grid(column=3, row=8, padx=(20,0), pady=(20,0))
 
-        self.maintenance_vin_label = Label(self, text="Enter the VIN of the vehicle being serviced: ")
-        self.maintenance_vin_label.grid(column=2, row = 8, padx=(20,0), pady=(20,0))
-        self.maintenance_vin_entry = Entry(self, width=20, textvariable=self.vin)
-        self.maintenance_vin_entry.grid(column=3, row=8, padx=(20,0), pady=(20,0))
-        
-        self.maintenance_button = Button(self, text="Submit", command=self.create_Maintenance)
-        self.maintenance_button.grid(column=2, row=10, padx=(20,0), pady=(20,0))
-
-        self.maintenance_button_del = Button(self, text="Delete", command=self.delete_Maintenance)
-
-    def create_Maintenance(self):
-        #create date object
-        self.date = datetime.date(self.year.get(), self.month.get(), self.day.get())
-        add_maintenance(self.connection, self.main_no.get(), self.date, self.service.get(), self.e_id.get())
-        add_hasMaintenance(self.connection, self.vin.get(), self.main_no.get())
-
-    def delete_Maintenance(self):
-        delete_maintenance(self.connection, self.main_no.get())
